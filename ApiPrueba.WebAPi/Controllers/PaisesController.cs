48fd7153f3b6037406412e3d80a3990e611b8ad2
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2020-05-12 14:17:03 -0300

    Primer Controler

---
+++ ApiPrueba.WebAPi/Controllers/PaisesController.cs
@@ -0,0 +1,28 @@
+﻿using ApiPrueba.WebAPi.Data;
+using ApiPrueba.WebAPi.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPrueba.WebAPi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly ApiContext context;
+
+        public PaisesController(ApiContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Pais>> Get()
+        {
+            return context.Paises.ToList();
+        }
+    }
+}
